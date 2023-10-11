e16892f81e6ffc66f0ae71c2333230a061670993
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-10-10 23:12:40 -0600

    Agregando practica 1

---
+++ JS/Practica1-Parcial3.js
@@ -0,0 +1,31 @@
+let $h1 = document.querySelector("h1");
+$h1.innerText = "Edgar Adrián Pinto Aquino";
+$h1.style.fontFamily= "Courier";
+$h1.style.color = "Green";
+$h1.style.backgroundColor = "Black";
+
+let $img = document.querySelector("img");
+$img.src = "https://cdn.discordapp.com/attachments/1013548438438301887/1161525871698522203/Practica1.jpg?ex=65389e43&is=65262943&hm=0e2e3b0a2c577d51c94788e84ac43e012f34fa0c189f98adfbfc11a68d9c932a&";
+$img.style.borderRadius="50%";
+$img.style.width= "400px";
+$img.style.height = "600px";
+
+let $p = document.querySelector("p");
+$p.innerText = " Naci el 01 de Febrero de 2003 en el puerto de Veracruz, curse desde la primaria hasta el bachillerato en Veracruz pero en el momento de elegir carrera decidi mudarme a CDMX debido a su oferta academica que no se podia encontrar en Veracruz, actualmente estoy estudiando la Ingenieria en Desarrollo de software Interactivo y Videojuegos, por otro lado en mi trayectoria en el deporte emepece a practicar artes marciales desde los 10 años, empece con kung fu y actualmente entreno Lima Lama y Kick Boxing.";
+$p.style.fontFamily= "Verdana";
+$p.style.color = "White";
+$p.style.backgroundColor = "Gray";
+
+let $ol = document.querySelector("ol");
+$ol.innerHTML = "<li>Practicar artes marciales</li><li>Leer sobre historia de Mexico y universal</li><li>Escuchar musica</li><li>Dormir</li><li>Meditar</li>"
+$ol.style.fontFamily= "Arial";
+$ol.style.color = "Cyan";
+$ol.style.backgroundColor = "Black";
+
+let $ul = document.querySelector("ul");
+$ul.innerHTML = "<li>Correo: edgarpinto010203@gmail.com</li><li>Telefono: 5562013401</li>"
+$ul.style.fontFamily= "Arial";
+$ul.style.color = "Yellow";
+$ul.style.backgroundColor = "Black";
+
+
